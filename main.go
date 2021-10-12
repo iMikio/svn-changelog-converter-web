656272b60b964f15ccaa3b7d1ffe177082ec53cf
iMikio <mkio.vo@gmail.com>
2021-10-12 17:44:12 +0900

    ログ管理追加

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"strings"
+
+	"github.com/beego/beego/v2/core/logs"
 	beego "github.com/beego/beego/v2/server/web"
 	_ "github.com/iMikio/SVNChangeLogToCSV-web/routers"
 )
 
 func main() {
 	beego.Run()
+}
+
+func init() {
+	runmode := beego.BConfig.RunMode
+	if runmode == "prod" {
+		config := `{
+			"filename":"logs/appname.log",
+			"separate":["emergency", "alert", "critical", "error", "warning", "notice", "info", "debug"],
+			"maxdays":90
+			}`
+		config = strings.Replace(config, "appname", beego.BConfig.AppName, 1)
+		logs.SetLogger(logs.AdapterMultiFile, config)
+	}
+	logs.Async()
+	logs.Info("runmode is %v", runmode)
 }
