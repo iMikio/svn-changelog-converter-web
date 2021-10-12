d74bfe94a005fe8b38e0cd314441dfa7d6c52de3
iMikio <mkio.vo@gmail.com>
2021-10-12 16:59:44 +0900

    beego デフォルト

---
+++ routers/router.go
@@ -0,0 +1,11 @@
+package routers
+
+import (
+	"github.com/iMikio/SVNChangeLogToCSV-web/controllers"
+
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+func init() {
+	beego.Router("/", &controllers.MainController{})
+}
